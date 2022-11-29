f7d2c4d7086e3ed0b61e12480b8dd6120ef7b685
zmzhoi <zmzhoi@gmail.com>
2022-11-29 13:43:21 +0900

    Fix typo.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended'],
   env: {
     commonjs: true,
     node: true,
@@ -8,5 +8,4 @@ module.exports = {
   rules: {
     'no-unused-vars': 'warn',
   },
-  ignorePatterns: ['dist'],
 };
